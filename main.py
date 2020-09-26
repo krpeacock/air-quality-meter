3095e787890383e0db2a85b0c06e43651b1c63a7
krpeacock <kylpeacock@gmail.com>
2020-09-26 15:43:07 -0700

    typo

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ while True:
         if int(maxAQI) < 50:
           pixels.fill((0, 228, 1))
 
-        out = f'{maxAQI},${next_25},{next_10}\n'
+        out = f'{maxAQI},{next_25},{next_10}\n'
 
         if displayReady:
             if out != prev_out:
